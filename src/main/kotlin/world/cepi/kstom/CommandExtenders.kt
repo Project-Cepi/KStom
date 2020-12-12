953d34dfae60496f3d4ebf8d3b8ce5995f95a8a2
LeoDog896 <LeoDog896@hotmail.com>
2020-12-12 16:26:06 -0500

    Argumenet callback helpers

--- src/main/kotlin/world/cepi/kstom/CommandExtenders.kt
+++ src/main/kotlin/world/cepi/kstom/CommandExtenders.kt
@@ -1,9 +1,12 @@
 package world.cepi.kstom
 
 import net.minestom.server.command.CommandSender
+import net.minestom.server.command.builder.ArgumentCallback
 import net.minestom.server.command.builder.Arguments
 import net.minestom.server.command.builder.Command
 import net.minestom.server.command.builder.arguments.Argument
+import net.minestom.server.command.builder.condition.CommandCondition
+import org.jetbrains.annotations.NotNull
 
 fun Command.addSyntax(lambda: () -> Unit) {
     this.setDefaultExecutor { _, _ -> lambda.invoke()}
@@ -27,4 +30,20 @@ fun Command.addSyntax(vararg arguments: Argument<*>, lambda: (sender: CommandSen
 
 fun Command.addSyntax(vararg arguments: Argument<*>, lambda: (sender: CommandSender, args: Arguments) -> Unit) {
     this.addSyntax({ sender, args -> lambda.invoke(sender, args)}, *arguments)
+}
+
+fun Command.setArgumentCallback(arg: Argument<*>, lambda: () -> Unit) {
+    this.setArgumentCallback({ _, _, _ -> lambda.invoke() }, arg)
+}
+
+fun Command.setArgumentCallback(arg: Argument<*>, lambda: (source: CommandSender) -> Unit) {
+    this.setArgumentCallback({ source, _, _ -> lambda.invoke(source) }, arg)
+}
+
+fun Command.setArgumentCallback(arg: Argument<*>, lambda: (source: CommandSender, value: String) -> Unit) {
+    this.setArgumentCallback({ source, value, _ -> lambda.invoke(source, value) }, arg)
+}
+
+fun Command.setArgumentCallback(arg: Argument<*>, lambda: (source: CommandSender, value: String, error: Int) -> Unit) {
+    this.setArgumentCallback({ source, value, error -> lambda.invoke(source, value, error) }, arg)
 }
