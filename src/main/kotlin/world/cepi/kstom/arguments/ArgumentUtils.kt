1bdc68b303aad0abec4abaf92415c1aff290054b
LeoDog896 <LeoDog896@hotmail.com>
2020-12-15 17:46:10 -0500

    Add string as subcommand

--- src/main/kotlin/world/cepi/kstom/arguments/ArgumentUtils.kt
+++ src/main/kotlin/world/cepi/kstom/arguments/ArgumentUtils.kt
@@ -2,10 +2,13 @@ package world.cepi.kstom.arguments
 
 import net.minestom.server.MinecraftServer
 import net.minestom.server.command.builder.Arguments
+import net.minestom.server.command.builder.arguments.ArgumentType
 import net.minestom.server.command.builder.arguments.ArgumentWord
 import net.minestom.server.entity.Player
 
 fun ArgumentWord.asSubcommand(): ArgumentWord = this.from(this.id)
+fun String.asSubcommand(): ArgumentWord = ArgumentType.Word(this).from(this)
+
 
 fun Arguments.getPlayer(id: String): Player {
 
