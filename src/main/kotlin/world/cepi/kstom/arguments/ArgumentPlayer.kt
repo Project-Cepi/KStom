e6bcc1bd428a714d60ecb33ed8d5bba912b6fa39
LeoDog896 <LeoDog896@hotmail.com>
2021-01-14 14:07:16 -0500

    *why was i using a java interface*

--- src/main/kotlin/world/cepi/kstom/arguments/ArgumentPlayer.kt
+++ src/main/kotlin/world/cepi/kstom/arguments/ArgumentPlayer.kt
@@ -2,16 +2,30 @@ package world.cepi.kstom.arguments
 
 import net.minestom.server.MinecraftServer
 import net.minestom.server.command.builder.Arguments
-import net.minestom.server.command.builder.arguments.ArgumentType
+import net.minestom.server.command.builder.arguments.Argument
 import net.minestom.server.command.builder.arguments.ArgumentWord
 import net.minestom.server.entity.Player
+import net.minestom.server.entity.fakeplayer.FakePlayer
+import java.util.*
 
-class ArgumentPlayer(id: String) : ArgumentWord(id) {
-    override fun from(vararg restrictions: String?): ArgumentWord = this
-    override fun getRestrictions(): Array<String> {
-        val restrictions: MutableList<String> = mutableListOf()
-        MinecraftServer.getConnectionManager().onlinePlayers.forEach { restrictions.add(it.username) }
-        return restrictions.toTypedArray()
+class ArgumentPlayer(id: String) : Argument<Player>(id) {
+    override fun getCorrectionResult(value: String): Int {
+        return if (value.contains(" ")) ArgumentWord.SPACE_ERROR else SUCCESS
+    }
+
+    override fun parse(value: String): Player {
+        return MinecraftServer.getConnectionManager().onlinePlayers.first { it.username == value }
+    }
+
+    fun getConditionResult(value: String): Int {
+        return if (MinecraftServer.getConnectionManager().onlinePlayers.any { it.username == value })
+            SUCCESS
+        else
+            ArgumentWord.RESTRICTION_ERROR
+    }
+
+    override fun getConditionResult(player: Player): Int {
+        return 0
     }
 }
 
