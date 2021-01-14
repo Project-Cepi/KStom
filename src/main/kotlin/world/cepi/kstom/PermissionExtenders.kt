ba75c94151d8316d2bfa11b25c250a2fc0f11099
LeoDog896 <LeoDog896@hotmail.com>
2021-01-14 14:08:32 -0500

    Use optional param

--- src/main/kotlin/world/cepi/kstom/PermissionExtenders.kt
+++ src/main/kotlin/world/cepi/kstom/PermissionExtenders.kt
@@ -5,16 +5,9 @@ import net.minestom.server.permission.Permission
 import org.jglrxavpok.hephaistos.nbt.NBTCompound
 
 /**
- * Adds a string permission to the player without the object overhead.
- *
- * @param string The string to add to the player's permissions.
- */
-fun Player.addPermission(string: String) = this.addPermission(Permission(string))
-
-/**
  * Adds a string permission with NBT to the player without the object overhead.
  *
  * @param string The string to add to the player's permissions.
  * @param nbt The additional NBT data to add to the permission.
  */
-fun Player.addPermission(string: String, nbt: NBTCompound) = this.addPermission(Permission(string, nbt))
+fun Player.addPermission(string: String, nbt: NBTCompound? = null) = this.addPermission(Permission(string, nbt))
