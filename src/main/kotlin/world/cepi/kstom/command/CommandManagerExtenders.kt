d8d45c04f63401a78404436ad092fcf7b8e3c1d6
plony <esiredrise@gmail.com>
2021-02-01 06:31:48 +0600

    "register" renamed to "command"

--- src/main/kotlin/world/cepi/kstom/command/CommandManagerExtenders.kt
+++ src/main/kotlin/world/cepi/kstom/command/CommandManagerExtenders.kt
@@ -5,7 +5,7 @@ import net.minestom.server.command.CommandSender
 import net.minestom.server.command.builder.Arguments
 import net.minestom.server.command.builder.Command
 
-inline fun CommandManager.register(name: String, block: Command.() -> Unit) =
+inline fun CommandManager.command(name: String, block: Command.() -> Unit) =
     register(Command(name).apply(block))
 
 fun CommandManager.register(name: String, block: (sender: CommandSender, args: Arguments) -> Unit) =
