493fa0fd43515168888a3e9741fbf9af7be7562b
plony <esiredrise@gmail.com>
2021-02-11 20:46:42 +0600

    Refactored KItem, added `events` in CoroutinesUtils.kt

--- src/main/kotlin/world/cepi/kstom/CoroutinesUtils.kt
+++ src/main/kotlin/world/cepi/kstom/CoroutinesUtils.kt
@@ -3,9 +3,24 @@ package world.cepi.kstom
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExecutorCoroutineDispatcher
 import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import net.minestom.server.MinecraftServer
+import net.minestom.server.event.Event
 import java.util.concurrent.Executors
 
 public val IOContext: ExecutorCoroutineDispatcher = Executors
     .newFixedThreadPool(Runtime.getRuntime().availableProcessors())
     .asCoroutineDispatcher()
 public val IOScope: CoroutineScope = CoroutineScope(IOContext)
+
+private const val eventBufferSize = 100
+
+/** Flow of all events */
+public val events: SharedFlow<Event> by lazy {
+    MutableSharedFlow<Event>(eventBufferSize).also { flow ->
+        MinecraftServer.getGlobalEventHandler().addEventCallback<Event> {
+            flow.emit(this)
+        }
+    }
+}
