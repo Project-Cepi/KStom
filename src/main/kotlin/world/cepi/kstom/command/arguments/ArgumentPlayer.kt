25225cadeb153f4c7265529bc30158f8effc82d2
LeoDog896 <LeoDog896@hotmail.com>
2021-02-13 09:07:42 -0500

    Add proper methods to arguments

--- src/main/kotlin/world/cepi/kstom/command/arguments/ArgumentPlayer.kt
+++ src/main/kotlin/world/cepi/kstom/command/arguments/ArgumentPlayer.kt
@@ -2,9 +2,14 @@ package world.cepi.kstom.command.arguments
 
 import net.minestom.server.MinecraftServer
 import net.minestom.server.command.builder.Arguments
+import net.minestom.server.command.builder.NodeMaker
 import net.minestom.server.command.builder.arguments.Argument
+import net.minestom.server.command.builder.arguments.minecraft.SuggestionType
 import net.minestom.server.command.builder.exception.ArgumentSyntaxException
 import net.minestom.server.entity.Player
+import net.minestom.server.network.packet.server.play.DeclareCommandsPacket
+import net.minestom.server.utils.binary.BinaryWriter
+import java.util.function.Consumer
 
 public class ArgumentPlayer(id: String) : Argument<Player>(id, false, false) {
 
@@ -15,6 +20,18 @@ public class ArgumentPlayer(id: String) : Argument<Player>(id, false, false) {
         } catch (e: Exception) {
             throw ArgumentSyntaxException("The player does not exist!", value, 1)
         }
+    }
+
+    override fun processNodes(nodeMaker: NodeMaker, executable: Boolean) {
+        val argumentNode = simpleArgumentNode(this, executable, false, true)
+
+        argumentNode.parser = "brigadier:string"
+        argumentNode.properties = Consumer { packetWriter: BinaryWriter ->
+            packetWriter.writeVarInt(0) // Single word
+        }
+        argumentNode.suggestionsType = SuggestionType.ASK_SERVER.identifier
+
+        nodeMaker.addNodes(arrayOf(argumentNode))
     }
 }
 
