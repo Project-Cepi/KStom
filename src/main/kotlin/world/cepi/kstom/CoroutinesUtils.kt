896503f11ea44e0ab9c50435869c6d5d93c29291
plony <esiredrise@gmail.com>
2021-04-03 22:32:39 +0300

    Suspended extension functions added to replace callback functions. Added miscellaneous extension functions

--- src/main/kotlin/world/cepi/kstom/CoroutinesUtils.kt
+++ src/main/kotlin/world/cepi/kstom/CoroutinesUtils.kt
@@ -3,13 +3,44 @@ package world.cepi.kstom
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExecutorCoroutineDispatcher
 import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import net.minestom.server.MinecraftServer
-import net.minestom.server.event.Event
+import net.minestom.server.entity.Entity
+import net.minestom.server.entity.Player
+import net.minestom.server.entity.fakeplayer.FakePlayer
+import net.minestom.server.entity.fakeplayer.FakePlayerOption
+import net.minestom.server.instance.Chunk
+import net.minestom.server.instance.IChunkLoader
+import net.minestom.server.instance.Instance
+import net.minestom.server.instance.InstanceContainer
+import net.minestom.server.instance.batch.BlockBatch
+import net.minestom.server.utils.Position
+import java.util.*
 import java.util.concurrent.Executors
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 public val IOContext: ExecutorCoroutineDispatcher = Executors
     .newFixedThreadPool(Runtime.getRuntime().availableProcessors())
     .asCoroutineDispatcher()
 public val IOScope: CoroutineScope = CoroutineScope(IOContext)
+
+
+public suspend fun FakePlayer(uuid: UUID, username: String, option: FakePlayerOption = FakePlayerOption()): FakePlayer = suspendCoroutine { cont ->
+    FakePlayer.initPlayer(uuid, username, option) {
+        cont.resume(it)
+    }
+}
+
+public suspend fun Entity.waitNextTick(): Entity = suspendCoroutine { cont -> scheduleNextTick { cont.resume(it) } }
+
+public suspend fun Entity.suspendTeleport(position: Position): Unit = suspendCoroutine { teleport(position) { it.resume(Unit) } }
+public suspend fun Player.suspendTeleport(position: Position): Unit = suspendCoroutine { teleport(position) { it.resume(Unit) } }
+
+public suspend fun IChunkLoader.saveChunk(chunk: Chunk): Unit = suspendCoroutine { saveChunk(chunk) { it.resume(Unit) } }
+public suspend fun IChunkLoader.saveChunks(chunk: Collection<Chunk>): Unit = suspendCoroutine { saveChunks(chunk) { it.resume(Unit) } }
+
+public suspend fun Instance.suspendSaveChunkToStorage(chunk: Chunk): Unit = suspendCoroutine { saveChunkToStorage(chunk) { it.resume(Unit) } }
+public suspend fun Instance.suspendSaveChunksToStorage(): Unit = suspendCoroutine { saveChunksToStorage { it.resume(Unit) } }
+
+public suspend fun InstanceContainer.suspendSaveInstance(): Unit = suspendCoroutine { saveInstance { it.resume(Unit) } }
+
+public suspend fun BlockBatch.flush(shouldLoadChunks: Boolean = false): Unit = suspendCoroutine { flush({ it.resume(Unit) }, shouldLoadChunks) }
