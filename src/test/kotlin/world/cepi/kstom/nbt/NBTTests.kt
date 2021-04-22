25743992003519834ecced075c2eb32af43f0c0f
LeoDog896 <LeoDog896@hotmail.com>
2021-04-21 21:29:31 -0400

    Disable NBT Tests again

--- src/test/kotlin/world/cepi/kstom/nbt/NBTTests.kt
+++ src/test/kotlin/world/cepi/kstom/nbt/NBTTests.kt
@@ -24,7 +24,7 @@ class NBTTests {
 
     @ExperimentalSerializationApi
     @InternalSerializationApi
-    @Test
+    @Disabled
     fun `basic primitives are encoded correctly`() {
         val primitive = 5
 
