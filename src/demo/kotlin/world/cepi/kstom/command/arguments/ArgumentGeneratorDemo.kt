721c696099a671c7e55311faa0a6885a45150925
LeoDog896 <LeoDog896@hotmail.com>
2021-05-02 16:16:03 -0400

    Rename variables correctly

--- src/demo/kotlin/world/cepi/kstom/command/arguments/ArgumentGeneratorDemo.kt
+++ src/demo/kotlin/world/cepi/kstom/command/arguments/ArgumentGeneratorDemo.kt
@@ -14,11 +14,11 @@ data class CoolItem(
 object ArgumentGeneratorDemo : Command("argumentGeneratorDemo") {
 
     init {
-        val classArgs = argumentsFromClass<CoolItem>()
-        val alternativeClassArgs = argumentsFromClass(CoolItem::class)
+        val argumentGenerator = argumentsFromClass<CoolItem>()
+        val alternativeargumentGenerator = argumentsFromClass(CoolItem::class)
 
-        addSyntax(*classArgs.args) { sender, args ->
-            val obj = classArgs.createInstance(args)
+        addSyntax(*argumentGenerator.args) { sender, args ->
+            val obj = argumentGenerator.createInstance(args)
 
             sender.sendMessage(obj.toString())
         }
