f1b216124822d5a99e5ee183b30272a0af6184ad
CmdrNorthpaw <thetuxkid@gmail.com>
2021-05-04 10:01:35 +0100

    NBT serializer yay

---
+++ src/main/kotlin/world/cepi/kstom/nbt/NBTSerializer.kt
@@ -0,0 +1,28 @@
+package world.cepi.kstom.nbt
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import org.jglrxavpok.hephaistos.nbt.NBT
+import org.jglrxavpok.hephaistos.nbt.SNBTParser
+import java.io.StringReader
+
+object NBTSerializer : KSerializer<NBT> {
+    override fun deserialize(decoder: Decoder): NBT {
+        return SNBTParser(StringReader(decoder.decodeSerializableValue(SerializableNBT.serializer()).nbt)).parse()
+    }
+
+    override val descriptor: SerialDescriptor
+        get() = SerializableNBT.serializer().descriptor
+
+    override fun serialize(encoder: Encoder, value: NBT) {
+        encoder.encodeSerializableValue(SerializableNBT.serializer(), SerializableNBT(value.toSNBT()))
+    }
+
+    fun NBT.serializer() = this@NBTSerializer
+    // Inline class because stdlib is weirdly outdated
+    @Serializable
+    private inline class SerializableNBT(val nbt: String)
+}
