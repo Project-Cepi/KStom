361aa59c2a07abddcdbe8328909ed7364c519935
LeoDog896 <LeoDog896@hotmail.com>
2021-08-08 11:21:00 -0400

    Update ArgumentPlayer.kt

--- src/main/kotlin/world/cepi/kstom/command/arguments/ArgumentPlayer.kt
+++ src/main/kotlin/world/cepi/kstom/command/arguments/ArgumentPlayer.kt
@@ -5,5 +5,5 @@ import net.minestom.server.command.builder.exception.ArgumentSyntaxException
 import world.cepi.kstom.Manager
 
 fun ArgumentPlayer(id: String) = ArgumentWord(id)
-    .map { Manager.connection.getPlayer(id) ?: throw ArgumentSyntaxException("Invalid player", it, 1) }
-    .suggest { Manager.connection.onlinePlayers.map { it.username } }
+    .map { Manager.connection.getPlayer(it) ?: throw ArgumentSyntaxException("Invalid player", it, 1) }
+    .suggest(SuggestionIgnoreOption.IGNORE_CASE) { Manager.connection.onlinePlayers.map { it.username } }
