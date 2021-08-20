4d5b02905d3519cc8de05896b10c914f938c0516
LeoDog896 <LeoDog896@hotmail.com>
2021-08-19 21:19:50 -0400

    Accept point instead of vec/pos

--- src/main/kotlin/world/cepi/kstom/util/AudienceUtils.kt
+++ src/main/kotlin/world/cepi/kstom/util/AudienceUtils.kt
@@ -2,23 +2,13 @@ package world.cepi.kstom.util
 
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.sound.Sound
-import net.minestom.server.coordinate.Pos
-import net.minestom.server.coordinate.Vec
+import net.minestom.server.coordinate.Point
 
 /**
  * Plays a sound to a specified [Audience]
  *
  * @param sound The sound to play
- * @param position The [Pos] to play the sound at
+ * @param position The [Point] to play the sound at
  */
-fun Audience.playSound(sound: Sound, position: Pos) =
+fun Audience.playSound(sound: Sound, position: Point) =
     playSound(sound, position.x(), position.y(), position.z())
-
-/**
- * Plays a sound to a specified [Audience]
- *
- * @param sound The sound to play
- * @param vector The [Vec] to play the sound at
- */
-fun Audience.playSound(sound: Sound, vector: Vec) =
-    playSound(sound, vector.x(), vector.y(), vector.z())
