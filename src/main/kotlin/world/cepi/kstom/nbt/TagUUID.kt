4c1482be80483b708289b64bd02243695ca59326
LeoDog896 <LeoDog896@hotmail.com>
2021-10-02 12:04:05 -0400

    Switch to actual tag

--- src/main/kotlin/world/cepi/kstom/nbt/TagUUID.kt
+++ src/main/kotlin/world/cepi/kstom/nbt/TagUUID.kt
@@ -1,30 +1,22 @@
 package world.cepi.kstom.nbt
 
 import net.minestom.server.tag.Tag
-import net.minestom.server.tag.TagReadable
-import net.minestom.server.tag.TagSerializer
-import net.minestom.server.tag.TagWritable
 import java.util.*
+import java.util.function.BiConsumer
 
-class TagUUID(val name: String) : TagSerializer<UUID> {
+class TagUUID(val name: String) : Tag<UUID>(name,
+    get@ { compound ->
+        val array = compound.getLongArray(name) ?: return@get null
 
-    override fun read(reader: TagReadable): UUID? {
-        val array = reader.getTag(Tag.LongArray(name)) ?: return null
-
-        return UUID(
-            array.getOrNull(0) ?: return null,
-            array.getOrNull(1) ?: return null
+        return@get UUID(
+            array.getOrNull(0) ?: return@get null,
+            array.getOrNull(1) ?: return@get null
         )
-    }
-
-    override fun write(writer: TagWritable, value: UUID?) {
-
-        if (value == null) return
-
-        writer.setTag(Tag.LongArray(name), LongArray(2).also {
+    },
+    BiConsumer { compound, value ->
+        compound.setLongArray(name, LongArray(2).also {
             it[0] = value.leastSignificantBits
             it[1] = value.mostSignificantBits
         })
     }
-
-}
+)
