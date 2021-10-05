038f0398dd8e09efbc83aecb776bf0216264a992
LeoDog896 <LeoDog896@hotmail.com>
2021-10-05 15:59:12 -0400

    Better TagUUID
    thx themode

--- src/main/kotlin/world/cepi/kstom/nbt/TagUUID.kt
+++ src/main/kotlin/world/cepi/kstom/nbt/TagUUID.kt
@@ -2,21 +2,18 @@ package world.cepi.kstom.nbt
 
 import net.minestom.server.tag.Tag
 import java.util.*
-import java.util.function.BiConsumer
 
-class TagUUID(val name: String) : Tag<UUID>(name,
-    get@ { compound ->
-        val array = compound.getLongArray(name) ?: return@get null
-
+fun TagUUID(name: String) = Tag.LongArray(name).map<UUID>(
+    get@ { map ->
         return@get UUID(
-            array.getOrNull(0) ?: return@get null,
-            array.getOrNull(1) ?: return@get null
+            map.getOrNull(0) ?: return@get null,
+            map.getOrNull(1) ?: return@get null
         )
     },
-    BiConsumer { compound, value ->
-        compound.setLongArray(name, LongArray(2).also {
-            it[0] = value.mostSignificantBits
+    set@ { value ->
+        LongArray(2).also {
+            it[0] = value?.mostSignificantBits ?: return@set null
             it[1] = value.leastSignificantBits
-        })
+        }
     }
 )
