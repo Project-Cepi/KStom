540ae58d5d7f9f6913affd92c18be37c0ee2a328
christian162 <christian.sheridan151@gmail.com>
2021-10-29 13:05:53 -0500

    Update block serializer

--- src/main/kotlin/world/cepi/kstom/serializer/BlockSerializer.kt
+++ src/main/kotlin/world/cepi/kstom/serializer/BlockSerializer.kt
@@ -1,37 +1,45 @@
 package world.cepi.kstom.serializer
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
 import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.element
+import kotlinx.serialization.encoding.*
 import net.minestom.server.instance.block.Block
 import net.minestom.server.utils.NamespaceID
 import org.jglrxavpok.hephaistos.nbt.NBT
 import org.jglrxavpok.hephaistos.nbt.NBTCompound
 
-@Serializable
-internal data class BlockSurrogate(
-    @Serializable(with = NamespaceIDSerializer::class)
-    val namespaceId: NamespaceID,
-    @Serializable(with = NBTSerializer::class)
-    val blockNbt: NBT
-)
-
+@Serializer(forClass = Block::class)
+@OptIn(ExperimentalSerializationApi::class)
 object BlockSerializer : KSerializer<Block> {
-    override val descriptor: SerialDescriptor = BlockSurrogate.serializer().descriptor
-
-    override fun deserialize(decoder: Decoder): Block {
-        val blockSurrogate = decoder.decodeSerializableValue(BlockSurrogate.serializer())
-
-        val nbtCompound = blockSurrogate.blockNbt as? NBTCompound
-        val block = Block.fromNamespaceId(blockSurrogate.namespaceId)
-
-        return block?.let { block.withNbt(nbtCompound) } ?: Block.AIR
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Block") {
+        element("namespace", NamespaceIDSerializer.descriptor)
+        element<NBT>("nbt")
     }
 
     override fun serialize(encoder: Encoder, value: Block) {
-        val blockSurrogate = BlockSurrogate(value.namespace(), value.nbt() ?: NBTCompound())
-        encoder.encodeSerializableValue(BlockSurrogate.serializer(), blockSurrogate)
+        encoder.encodeStructure(descriptor) {
+            encodeSerializableElement(descriptor, 0, NamespaceIDSerializer, value.namespace())
+            encodeSerializableElement(descriptor, 1, NBTSerializer, value.nbt() ?: NBTCompound() as NBT)
+        }
+    }
+
+    override fun deserialize(decoder: Decoder): Block = decoder.decodeStructure(descriptor) {
+        var namespaceID: NamespaceID? = null
+        var nbt: NBTCompound? = null
+
+        while (true) {
+            when (val index = decodeElementIndex(descriptor)) {
+                0 -> namespaceID = decodeSerializableElement(descriptor, 0, NamespaceIDSerializer)
+                1 -> nbt = decodeSerializableElement(descriptor, 1, NBTSerializer) as? NBTCompound ?: NBTCompound()
+                CompositeDecoder.DECODE_DONE -> break
+                else -> error("Unexpected index: $index")
+            }
+        }
+
+        namespaceID?.let { Block.fromNamespaceId(it)?.withNbt(nbt) } ?: Block.AIR
     }
 }
