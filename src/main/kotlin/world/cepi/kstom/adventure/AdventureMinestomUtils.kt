e709b88a7769ee8dc2be17b3a9cb4bfffe14b337
christian162 <christian.sheridan151@gmail.com>
2021-10-29 13:11:41 -0500

    Add placeholders to MiniMessage#parse

--- src/main/kotlin/world/cepi/kstom/adventure/AdventureMinestomUtils.kt
+++ src/main/kotlin/world/cepi/kstom/adventure/AdventureMinestomUtils.kt
@@ -6,7 +6,7 @@ import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 
-public fun String.asMini(placeholders: Map<String, String> = mapOf()): Component = MiniMessage.get().parse(this)
+public fun String.asMini(placeholders: Map<String, String> = mapOf()): Component = MiniMessage.get().parse(this, placeholders)
 
 public fun Audience.sendMiniMessage(miniMessage: String, placeholders: Map<String, String> = mapOf()): Unit =
     this.sendMessage(miniMessage.asMini(placeholders))
