a7f43f0a2c5568c89faf5165453293545c373676
LeoDog896 <LeoDog896@gmail.com>
2022-01-16 18:53:30 -0500

    Cleanup

--- src/main/kotlin/world/cepi/kstom/command/arguments/generation/ChosenArgumentGeneration.kt
+++ src/main/kotlin/world/cepi/kstom/command/arguments/generation/ChosenArgumentGeneration.kt
@@ -3,18 +3,19 @@ package world.cepi.kstom.command.arguments.generation
 import world.cepi.kstom.command.kommand.Kommand
 import kotlin.reflect.KClass
 import kotlin.reflect.full.companionObject
+import kotlin.reflect.full.companionObjectInstance
 import kotlin.reflect.full.isSubclassOf
 
 class ChosenArgumentGeneration<T : Any>(override val clazz: KClass<T>): ArgumentGenerator<T>(clazz, run {
-    if (clazz.companionObject!!::class.isSubclassOf(CustomArgumentGeneration::class)) {
-        (clazz.companionObject as CustomArgumentGeneration<*>).argumentsForGeneration
+    if (clazz.companionObjectInstance is CustomArgumentGeneration<*>) {
+        (clazz.companionObjectInstance as CustomArgumentGeneration<*>).argumentsForGeneration
     } else {
         generateSyntaxes(clazz)
     }
 }) {
     override fun generate(syntax: Kommand.SyntaxContext, args: List<String>, index: Int): T {
-        return if (clazz.companionObject!!::class.isSubclassOf(CustomArgumentGeneration::class)) {
-            (clazz.companionObject as CustomArgumentGeneration<T>).generate(syntax, args, index)
+        return if (clazz.companionObjectInstance is CustomArgumentGeneration<*>) {
+            (clazz.companionObjectInstance as CustomArgumentGeneration<T>).generate(syntax, args, index)
         } else {
             ClassArgumentGenerator(clazz).generate(syntax, args, index)
         }
