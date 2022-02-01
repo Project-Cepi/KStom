d9062b21fd8b165914897b00b68c5d427aaf9a48
Tristan <LeoDog896@hotmail.com>
2022-02-01 10:59:21 -0500

    Add base sidebar demo

---
+++ src/demo/kotlin/world/cepi/kstom/sidebar/SidebarDemo.kt
@@ -0,0 +1,35 @@
+package world.cepi.kstom.sidebar
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.minestom.server.entity.Player
+import net.minestom.server.event.Event
+import net.minestom.server.event.EventNode
+import net.minestom.server.event.player.PlayerStartFlyingEvent
+import net.minestom.server.event.player.PlayerStopFlyingEvent
+import world.cepi.kstom.adventure.asMini
+import world.cepi.kstom.event.listenOnly
+
+fun attatchSidebar(player: Player, node: EventNode<Event>) = sidebar(Component.text("Cool sidebar")) {
+    fun flying(isFlying: Boolean) = line("flying",2) {
+        Component.text("Flying: ", NamedTextColor.GRAY)
+            .append(Component.text(isFlying, NamedTextColor.WHITE))
+    }
+
+    val website = line("website", 0, Component.text("Cool Website"))
+
+    add(
+        flying(player.isFlying),
+        emptyLine("empty", 1),
+        website
+    )
+
+    node.listenOnly<PlayerStartFlyingEvent> {
+        refresh(flying(true))
+    }
+
+    node.listenOnly<PlayerStopFlyingEvent> {
+        refresh(flying(false))
+    }
+
+}
---
+++ src/main/kotlin/world/cepi/kstom/sidebar/Sidebar.kt
@@ -0,0 +1,32 @@
+package world.cepi.kstom.sidebar
+
+import net.kyori.adventure.text.Component
+import net.minestom.server.scoreboard.Sidebar
+import net.minestom.server.scoreboard.Sidebar.ScoreboardLine
+
+
+class KSidebar(title: Component) {
+    val internalSidebar = Sidebar(title)
+
+    inline fun line(id: String, index: Int, generationalLambda: () -> Component) = line(id, index, generationalLambda())
+
+    fun line(id: String, index: Int, component: Component) = ScoreboardLine(id, component, index)
+
+    fun emptyLine(id: String, index: Int) = line(id, index, Component.empty())
+
+    fun add(vararg lines: ScoreboardLine) = lines.forEach { line ->
+        internalSidebar.createLine(line)
+    }
+
+    fun refresh(vararg lines: ScoreboardLine) = lines.forEach { line ->
+        internalSidebar.updateLineContent(line.id, line.content)
+    }
+}
+
+fun sidebar(title: Component, lambda: KSidebar.() -> Unit): KSidebar {
+    val sidebar = KSidebar(title)
+
+    lambda(sidebar)
+
+    return sidebar
+}
