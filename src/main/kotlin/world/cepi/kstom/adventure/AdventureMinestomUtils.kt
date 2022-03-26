48b9446090467c4caaa7609148f4600d522275f7
Tristan <LeoDog896@hotmail.com>
2022-03-26 13:09:22 -0400

    More component utilities

--- src/main/kotlin/world/cepi/kstom/adventure/AdventureMinestomUtils.kt
+++ src/main/kotlin/world/cepi/kstom/adventure/AdventureMinestomUtils.kt
@@ -2,6 +2,7 @@ package world.cepi.kstom.adventure
 
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
@@ -15,3 +16,23 @@ fun Audience.sendMiniMessage(miniMessage: String, placeholders: Map<String, Stri
 fun Component.plainText(): String = PlainTextComponentSerializer.plainText().serialize(this)
 fun Component.legacyAmpersand(): String = LegacyComponentSerializer.legacyAmpersand().serialize(this)
 fun Component.noItalic() = decoration(TextDecoration.ITALIC, false)
+
+fun component(vararg components: Component) = components.fold(Component.text()) { acc, item ->
+    acc.append(item)
+}.build()
+
+fun String.color(color: TextColor) = Component.text(this, color)
+fun Int.color(color: TextColor) = Component.text(this, color)
+fun Byte.color(color: TextColor) = Component.text(this.toString(), color)
+fun Short.color(color: TextColor) = Component.text(this.toString(), color)
+fun Long.color(color: TextColor) = Component.text(this, color)
+fun Double.color(color: TextColor) = Component.text(this, color)
+fun Boolean.color(color: TextColor) = Component.text(this, color)
+
+fun String.asComponent() = Component.text(this)
+fun Int.asComponent() = Component.text(this)
+fun Byte.asComponent() = Component.text(this.toString())
+fun Short.asComponent() = Component.text(this.toString())
+fun Long.asComponent() = Component.text(this)
+fun Double.asComponent() = Component.text(this)
+fun Boolean.asComponent() = Component.text(this)
