cdd4c8ac7b568f5f4159470cb20db33e9f18f043
CatDevz <codyquinn1122@gmail.com>
2022-03-27 00:18:57 -0500

    Allow for a specific ItemMetaBuilder to optionally be passed into the item DSL

--- src/main/kotlin/world/cepi/kstom/item/KItem.kt
+++ src/main/kotlin/world/cepi/kstom/item/KItem.kt
@@ -3,12 +3,25 @@ package world.cepi.kstom.item
 import net.minestom.server.item.*
 
 /**
+ * DSL for Items with special meta.
+ *
+ * @param T The type of the item meta builder
+ *
+ * @param material The material of the item
+ * @param amount The amount of item to have
+ * @param init The DSL lambda
+ */
+fun <T : ItemMetaBuilder> item(material: Material = Material.PAPER, amount: Int = 1, init: T.() -> Unit = {}): ItemStack {
+    return ItemStack.of(material, amount).withMeta { metaBuilder: T -> init(metaBuilder); metaBuilder }
+}
+
+/**
  * DSL for Items.
  *
  * @param material The material of the item
  * @param amount The amount of item to have
  * @param init The DSL lambda
  */
-fun item(material: Material = Material.PAPER, amount: Int = 1, init: ItemMetaBuilder.() -> Unit = {}): ItemStack {
-    return ItemStack.of(material, amount).withMeta { metaBuilder: ItemMetaBuilder -> init(metaBuilder); metaBuilder }
-}
+@JvmName("itemDefault") // Doesn't matter because this library won't be used from Java
+fun item(material: Material = Material.PAPER, amount: Int = 1, init: ItemMetaBuilder.() -> Unit = {}): ItemStack =
+    item<ItemMetaBuilder>(material, amount, init)
