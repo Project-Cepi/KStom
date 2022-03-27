9fa417636b76356cd26ad273e49eb5fa1c281de2
Tristan <LeoDog896@hotmail.com>
2022-03-27 12:37:57 -0400

    Switch default asMini

--- src/main/kotlin/world/cepi/kstom/adventure/AdventureMinestomUtils.kt
+++ src/main/kotlin/world/cepi/kstom/adventure/AdventureMinestomUtils.kt
@@ -6,13 +6,15 @@ import net.kyori.adventure.text.ComponentBuilder
 import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.minimessage.template.TemplateResolver
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 
-fun String.asMini(placeholders: Map<String, String> = mapOf()): Component = MiniMessage.miniMessage().deserialize(this, placeholders)
+fun String.asMini(templateResolver: TemplateResolver = TemplateResolver.empty()): Component = MiniMessage.miniMessage()
+    .deserialize(this, templateResolver)
 
-fun Audience.sendMiniMessage(miniMessage: String, placeholders: Map<String, String> = mapOf()): Unit =
-    this.sendMessage(miniMessage.asMini(placeholders))
+fun Audience.sendMiniMessage(miniMessage: String, templateResolver: TemplateResolver = TemplateResolver.empty()): Unit =
+    this.sendMessage(miniMessage.asMini(templateResolver))
 
 fun Component.plainText(): String = PlainTextComponentSerializer.plainText().serialize(this)
 fun Component.legacyAmpersand(): String = LegacyComponentSerializer.legacyAmpersand().serialize(this)
