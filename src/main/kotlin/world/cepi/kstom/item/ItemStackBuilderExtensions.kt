cac886467c0ef8104c914195d0573e0efecf3828
CatDevz <codyquinn1122@gmail.com>
2022-03-29 23:01:42 -0500

    Added lore builder

--- src/main/kotlin/world/cepi/kstom/item/ItemStackBuilderExtensions.kt
+++ src/main/kotlin/world/cepi/kstom/item/ItemStackBuilderExtensions.kt
@@ -1,10 +1,13 @@
 package world.cepi.kstom.item
 
 import net.kyori.adventure.text.Component
-import net.minestom.server.item.ItemMeta
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 import net.minestom.server.item.ItemMetaBuilder
 import net.minestom.server.item.ItemStack
-import net.minestom.server.item.ItemStackBuilder
+
+fun ItemMetaBuilder.lore(tagResolver: TagResolver = TagResolver.empty(), builder: KLore.() -> Unit) {
+    this.lore = KLore().apply(builder).list.toList()
+}
 
 var ItemMetaBuilder.lore: List<Component>
     get() = emptyList()
@@ -22,7 +25,7 @@ var ItemMetaBuilder.unbreakable: Boolean
     get() = false
     set(value) = run { this.unbreakable(value) }
 
-fun ItemStack.and(amount: Int = -1, init: ItemMetaBuilder.() -> Unit) = this.with {
-    if (amount != -1) it.amount(amount)
+fun ItemStack.and(amount: Int? = null, init: ItemMetaBuilder.() -> Unit) = this.with {
+    if (amount != null) it.amount(amount)
     it.meta(meta.with(init))
 }
