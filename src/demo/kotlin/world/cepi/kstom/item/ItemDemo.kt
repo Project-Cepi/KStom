c71238a69f7cbed92b8c44eaee41efc86945ed7a
Tristan <LeoDog896@hotmail.com>
2022-05-22 10:04:25 -0400

    Add item demo

---
+++ src/demo/kotlin/world/cepi/kstom/item/ItemDemo.kt
@@ -0,0 +1,31 @@
+package world.cepi.kstom.item
+
+import net.kyori.adventure.text.Component
+import net.minestom.server.item.Material
+import net.minestom.server.item.metadata.WrittenBookMeta
+
+@kotlinx.serialization.Serializable
+class CoolClass(val num: Int, val coolProperty: String)
+
+val item = item<WrittenBookMeta.Builder, WrittenBookMeta>(material = Material.WRITTEN_BOOK, amount = 5) {
+    displayName = Component.text("Hey!")
+    displayName(Component.text("Hello!")) // or this
+
+    damage = 5
+    unbreakable = true
+
+    lore {
+        +"Hello"
+        +"<red>Minestom!</red>"
+    }
+
+    lore(Component.text("Hello!")) // or this
+
+    title("My first book")
+    author("Notch")
+    pages(Component.text("This is the first page"))
+
+    this["complexData"] = CoolClass(5, "Hello")
+}.withAmount(7).and {
+    displayName(Component.text("Hay!"))
+}
